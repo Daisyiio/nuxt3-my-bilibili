c85a4ecc051e2af4adcfaa07d3b3704d64f58e41
邹钰杰 <554091877@qq.com>
2025-02-05 13:13:42 +0800

    SEO

--- app.vue
+++ app.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+// SEO 优化
+  useSeoMeta({
+    title:'哔哩哔哩 (゜-゜)つロ 干杯~-bilibili',
+    description:'哔哩哔哩（bilibili.com)是国内知名的视频弹幕网站，这里有及时的动漫新番，活跃的ACG氛围，有创意的Up主。大家可以在这里找到许多欢乐。',
+    keywords:'B站，bilibili，哔哩哔哩，哔哩哔哩动画，动漫，电影，在线动漫，高清电影'
+  })
+</script>
+
 <template>
   <div>
     <NuxtLink to="/">首页</NuxtLink>
